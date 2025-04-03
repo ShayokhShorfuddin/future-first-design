19f57db4d25a722f23b948b916813d2dca2cb892
Shayokh <shayokhshuvro14@gmail.com>
2025-04-03 22:57:56 +0600

    remove unnecessary files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite({ target: 'react', autoCodeSplitting: true })],
 })
-
-// TODO: connect with github repo
