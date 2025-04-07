da79e165349046a0df516b041ad41d317fe5a769
Shayokh <shayokhshuvro14@gmail.com>
2025-04-08 05:46:49 +0600

    add vercel routing to fix 404 on page refresh issue

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -43,59 +43,63 @@ export default function Introduction() {
 					</p>
 					<br /> <br />
 					<div className="ml-6 md:ml-10">
-						<h2 className="text-3xl dark:text-neutral-200 font-inter font-semibold mb-6">
-							1. Low Resource Compatibility
-						</h2>
+						<div>
+							<h2 className="text-3xl dark:text-neutral-200 font-inter font-semibold mb-6">
+								1. Low Resource Compatibility
+							</h2>
 
-						<p className="dark:text-neutral-400 font-inter">
-							Majority of current web applications tend to have heavy
-							dependencies and often require more resources to function than
-							necessary. Unoptimized assets, large libraries, verbose
-							stylesheets, and excessive JavaScript, just to name a few.
-							<br /> <br />
-							All of your users may not have access to a decent machine or a
-							stable internet connection. Future-First Design philosophy reminds
-							that to developers. Applications must be designed in such a manner
-							that they achieve expected behavior using minimal resources and
-							limited bandwidth.
-							<br /> <br />
-							For instance, imagine a web application that depends on{" "}
-							<a className="underline" href="https://momentjs.com/">
-								moment.js
-							</a>{" "}
-							for date formatting. According to{" "}
-							<a
-								className="underline"
-								href="https://bundlephobia.com/package/moment@2.30.1"
-							>
-								bundlephobia
-							</a>
-							, it is 73.1 kB (minified + gzipped) in size and potentially takes
-							up to 1.46 seconds to be downloaded by the client's browser on
-							slow 3G network (50kB/s).
-							<br /> <br />
-							In this scenario, a better alternative would be to use{" "}
-							<a className="underline" href="https://day.js.org/">
-								day.js
-							</a>
-							, a lightweight 3 kB (minified + gzipped) library and takes only
-							up to 59ms on slow 3G network.
-						</p>
+							<p className="dark:text-neutral-400 font-inter">
+								Majority of current web applications tend to have heavy
+								dependencies and often require more resources to function than
+								necessary. Unoptimized assets, large libraries, verbose
+								stylesheets, and excessive JavaScript, just to name a few.
+								<br /> <br />
+								All of your users may not have access to a decent machine or a
+								stable internet connection. Future-First Design philosophy
+								reminds that to developers. Applications must be designed in
+								such a manner that they achieve expected behavior using minimal
+								resources and limited bandwidth.
+								<br /> <br />
+								For instance, imagine a web application that depends on{" "}
+								<a className="underline" href="https://momentjs.com/">
+									moment.js
+								</a>{" "}
+								for date formatting. According to{" "}
+								<a
+									className="underline"
+									href="https://bundlephobia.com/package/moment@2.30.1"
+								>
+									bundlephobia
+								</a>
+								, it is 73.1 kB (minified + gzipped) in size and potentially
+								takes up to 1.46 seconds to be downloaded by the client's
+								browser on slow 3G network (50kB/s).
+								<br /> <br />
+								In this scenario, a better alternative would be to use{" "}
+								<a className="underline" href="https://day.js.org/">
+									day.js
+								</a>
+								, a lightweight 3 kB (minified + gzipped) library and takes only
+								up to 59ms on slow 3G network.
+							</p>
 
-						<ul className="list-disc list-inside mt-7">
-							<li className="dark:text-neutral-400 font-inter">
-								Prune unnecessary dependencies and dead-code.
-							</li>
-							<li className="dark:text-neutral-400 font-inter">
-								Compress and optimize static assets.
-							</li>
-							<li className="dark:text-neutral-400 font-inter">
-								Ship as low JavaScript as possible.
-							</li>
-							<li className="dark:text-neutral-400 font-inter">
-								Make minimal network requests.
-							</li>
-						</ul>
+							<ul className="list-disc list-inside mt-7">
+								<li className="dark:text-neutral-400 font-inter">
+									Prune unnecessary dependencies and dead-code.
+								</li>
+								<li className="dark:text-neutral-400 font-inter">
+									Compress and optimize static assets.
+								</li>
+								<li className="dark:text-neutral-400 font-inter">
+									Ship as low JavaScript as possible.
+								</li>
+								<li className="dark:text-neutral-400 font-inter">
+									Make minimal network requests.
+								</li>
+							</ul>
+						</div>
+
+						{/* TODO: Add next value */}
 					</div>
 				</div>
 			</div>
